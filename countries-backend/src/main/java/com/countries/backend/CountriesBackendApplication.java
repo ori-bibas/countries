b87b7ff0bebb61f990807b6b9d2aa12e3f1047e2
Ori Bibas <oribibas@Oris-MacBook-Pro.local>
2025-04-28 17:56:07 -0400

    backend spring boot project

---
+++ countries-backend/src/main/java/com/countries/backend/CountriesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.countries.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountriesBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CountriesBackendApplication.class, args);
+    }
+
+}
